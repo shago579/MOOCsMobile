45e1ed5b40239a802d85028c6a13b44100bd7db6
Luis Santiago Vazquez Mancilla <shago579@gmail.com>
2014-04-26 22:31:21 -0500

    Login Image
    Imagenes para el Login listas, Tambien se configuro para que unicamente
    en esa activity pueda estar en modo portrait

--- gen/com/upch/moocsmobile/R.java
+++ gen/com/upch/moocsmobile/R.java
@@ -22,9 +22,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int login_image=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int touch_image=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,6 +38,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int login_picture=0x7f050003;
     }
     public static final class style {
         /** 
